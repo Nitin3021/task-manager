3bf387da74c5141e7acdab6398b4e6d16d5d9938
Nitin <nitinpillai30@yahoo.com>
2021-03-09 14:21:12 +0700

    Adding middleware logic for authorization

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const User = require('./models/user')
 const Task = require('./models/task')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const { response } = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled!')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('System under construction!')
+// })
 
 app.use(express.json())
 app.use(userRouter)
